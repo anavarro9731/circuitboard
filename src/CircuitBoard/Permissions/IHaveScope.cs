efdbcf35ac58141a57275c2be756b74714770731
Aaron Navarro <anavarro9731@gmail.com>
2019-11-22 14:06:12 +0000

    add scoping to permissions

---
+++ src/CircuitBoard/Permissions/IHaveScope.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace CircuitBoard.Permissions
+{
+    public interface IHaveScope
+    {
+        List<IScopeReference> ScopeReferences { get; set; }
+    }
+}
---
+++ src/CircuitBoard/Permissions/IScopeReference.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CircuitBoard.Permissions
+{
+    public interface IScopeReference
+    {
+        Guid ScopeObjectId { get; }
+
+        string ScopeObjectType { get; }
+
+        DateTime ScopeReferenceCreatedOn { get; }
+    }
+}
