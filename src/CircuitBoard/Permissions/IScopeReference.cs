0f325687e4b840beef96cac512c40f474c90e6c2
Aaron Navarro <anavarro9731@gmail.com>
2019-11-25 17:15:56 +0000

    Change some interfaces

--- src/CircuitBoard/Permissions/IScopeReference.cs
+++ src/CircuitBoard/Permissions/IScopeReference.cs
@@ -2,12 +2,84 @@
 
 namespace CircuitBoard.Permissions
 {
-    public interface IScopeReference
+    public class ScopeReference : IEquatable<ScopeReference>
     {
-        Guid ScopeObjectId { get; }
+        public ScopeReference(Guid idOfScope, string typeOfOwner, DateTime addedOn)
+        {
+            ScopeObjectId = idOfScope;
+            ScopeObjectType = typeOfOwner;
+            ScopeReferenceCreatedOn = addedOn;
+        }
 
-        string ScopeObjectType { get; }
+        public Guid ScopeObjectId { get; }
 
-        DateTime ScopeReferenceCreatedOn { get; }
+        public string ScopeObjectType { get; }
+
+        public DateTime ScopeReferenceCreatedOn { get; }
+
+        public static bool operator ==(ScopeReference a, ScopeReference b)
+        {
+            // If both are null, or both are same instance, return true.
+            if (ReferenceEquals(a, b))
+            {
+                return true;
+            }
+
+            // If one is null, but not both, return false.
+            if ((object)a == null || (object)b == null)
+            {
+                return false;
+            }
+
+            // Return true if the fields match:
+            return a.PropertiesAreEqual(b);
+        }
+
+        public static bool operator !=(ScopeReference a, ScopeReference b)
+        {
+            return !(a == b);
+        }
+
+        public override bool Equals(object obj)
+        {
+            //if the object passed is null return false;
+            if (ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+
+            //if the objects are the same instance return true
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+
+            //if the objects are of different types return false
+            if (obj.GetType() != GetType())
+            {
+                return false;
+            }
+
+            //check on property equality
+            return PropertiesAreEqual((ScopeReference)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            var hash = 13;
+            hash = hash * 7 + ScopeObjectId.GetHashCode();
+            hash = hash * 7 + ScopeObjectType.GetHashCode();
+            return hash;
+        }
+
+        bool IEquatable<ScopeReference>.Equals(ScopeReference other)
+        {
+            return Equals(other);
+        }
+
+        protected bool PropertiesAreEqual(ScopeReference other)
+        {
+            return ScopeObjectId.Equals(other.ScopeObjectId) && ScopeObjectType.Equals(other.ScopeObjectType);
+        }
     }
 }
