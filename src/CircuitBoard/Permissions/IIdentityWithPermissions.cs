c3b784322307864cdb829e9b10c7d3a92f18d0c6
Aaron Navarro <anavarro9731@gmail.com>
2019-12-13 14:46:23 +0000

    allow for specific types of permissions namely to support immature system.text.jsonserialiser

--- src/CircuitBoard/Permissions/IIdentityWithPermissions.cs
+++ src/CircuitBoard/Permissions/IIdentityWithPermissions.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 
 namespace CircuitBoard.Permissions
 {
-    public interface IIdentityWithPermissions
+    public interface IIdentityWithPermissions { }
+
+    public interface IIdentityWithPermissions<T> where T: IPermissionInstance, IIdentityWithPermissions
     {
         // ReSharper disable once InconsistentNaming
         Guid id { get; set; } //lowercase to support entities persisted in cosmosdb, grr!
 
-        List<IPermissionInstance> Permissions { get; set; }
+        List<T> Permissions { get; set; }
 
         string UserName { get; set; }
 
--- src/CircuitBoard/Permissions/IPermissionInstance.cs
+++ src/CircuitBoard/Permissions/IPermissionInstance.cs
@@ -1,20 +1,7 @@
-﻿using System;
-
-namespace CircuitBoard.Permissions
+﻿namespace CircuitBoard.Permissions
 {
     public interface IPermissionInstance : IPermission, IHaveScope
     {
 
-    }
-
-    public interface IPermission
-    {
-        int DisplayOrder { get; set; }
-
-        Guid Id { get; set; }
-
-        string PermissionName { get; set; }
-
-        Guid PermissionRequiredToAdministerThisPermission { get; set; }
     }
 }
