e4dafe00ef37c40c9fda1be476e81b3939544c16
aaron navarro <anavarro9731@gmail.com>
2020-06-24 17:47:50 +1000

    Add Permission Interface

---
+++ src/CircuitBoard/Security/IPermission.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CircuitBoard.Security
+{
+    public interface IPermission
+    {
+        int DisplayOrder { get; set; }
+
+        Guid Id { get; set; }
+
+        string PermissionName { get; set; }
+
+        Guid PermissionRequiredToAdministerThisPermission { get; set; }
+    }
