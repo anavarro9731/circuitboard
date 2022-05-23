28b667ea740a7b8a0021073677f2f558fe59e154
Aaron Navarro <anavarro9731@gmail.com>
2022-05-23 21:55:45 +0100

    fix silly error

--- src/CircuitBoard/CircuitException.cs
+++ src/CircuitBoard/CircuitException.cs
@@ -1,14 +1,11 @@
 using System;
-using System.Runtime.Serialization;
+
 
 namespace CircuitBoard
 {
     public class CircuitException : Exception
     {
-        protected CircuitException(SerializationInfo info, StreamingContext context) : base(info, context)
-        {
-        }
-
+        
         public CircuitException(string message) : base(message)
         {
         }
