341c30ab388f7416ee03fa580cc7be67b5ad40f4
Aaron Navarro <anavarro9731@gmail.com>
2022-06-26 12:35:22 +0100

    add optional guid param update assembly desc

--- src/CircuitBoard/CircuitException.cs
+++ src/CircuitBoard/CircuitException.cs
@@ -3,15 +3,18 @@ using System;
 
 namespace CircuitBoard
 {
-    public class CircuitException : Exception
+    public sealed class CircuitException : Exception
     {
         
-        public CircuitException(string message) : base(message)
+        public CircuitException(string message, Guid? code = null) : base(message)
         {
+            Data.Add("Code", code);
         }
 
-        public CircuitException(string message, Exception innerException) : base(message, innerException)
+        public CircuitException(string message, Exception innerException, Guid? code = null) : base(message, innerException)
         {
+            Data.Add("Code", code);
         }
     }
+    
 }
